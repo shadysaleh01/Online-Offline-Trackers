5b90494b084d48fe1c9aae2efeb2b56be74d189f
Shady Coder <shadysaleh01@gmail.com>
2020-11-03 15:28:26 -0800

    Added plugins with all data needed then create manifest file

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -9,6 +9,23 @@ const config = {
       path: __dirname + "/dist",
       filename: "app.bundle.js"
    },
+   mode: "development",
+   plugins: [
+      new WebpackPwaManifest({
+         fingerpints: false,
+         name: "Budget tracker app",
+         short_name: "Budget tracker",
+         description: "An application that allows user add expenses and deposits to their budget with or without a connection.",
+         start_url: "/",
+         icons: [
+            {
+               src: path.resolve("/icons"),
+               sizes: [96, 128, 192, 256, 384, 512],
+               destination: path.join("public", "icons"),
+            },
+         ],
+      }),
+   ],
 };
 
 module.exports = config;
