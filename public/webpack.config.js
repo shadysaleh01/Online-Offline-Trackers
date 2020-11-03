6afa7aedc52da1244e939a7ed07dc13a2aece0fe
Shady Coder <shadysaleh01@gmail.com>
2020-11-03 02:03:08 -0800

    Created dist file and convert index.js to bundle.js and exported the obj

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,2 +1,14 @@
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
+
+const config = {
+   entry: {
+      app: "./index.js"
+   },
+   output: {
+      path: __dirname + "/dist",
+      filename: "app.bundle.js"
+   },
+};
+
+module.exports = config;
