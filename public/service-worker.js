bc9a6e998558d6fe0ec31f0f6551c182a2884cc6
Shady Coder <shadysaleh01@gmail.com>
2020-11-03 17:16:55 -0800

    Created var that holds all file needed to cache in service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+const FILES_TO_CACHE = [
+   '/',
+   '/index.html',
+   '/styles.css',
+   '/dist/bundle.js',
+   'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+   'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+];
