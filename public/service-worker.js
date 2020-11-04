6e1ec0f69460c4df25bc123c7d04d715291cfba9
Shady Coder <shadysaleh01@gmail.com>
2020-11-03 17:50:32 -0800

    install Event listener for all cache files

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,3 +6,14 @@ const FILES_TO_CACHE = [
    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
 ];
+
+const PRECASHE = 'precache-v1';
+
+self.addEventListener('install', (event) => {
+   event.waitUntil(
+      caches
+         .open(PRECASHE)
+         .then((cache) => cache.addAll(FILES_TO_CACHE))
+         .then(self.skipWaiting())
+   )
+})
